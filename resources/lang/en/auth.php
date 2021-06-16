d0fd4509f083c23b50a50d30d3399ec38793926b
Mikhail <44241285+Mikhail-Mouner@users.noreply.github.com>
2021-06-16 20:49:38 +0200

    Add Lang en

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -16,5 +16,19 @@ return [
     'failed' => 'These credentials do not match our records.',
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'logout' => 'Logout',
+    'login' => 'Login',
+    'Register' => 'Register',
+    'sign-up' => 'Sign-Up',
+    'not authorized page' => 'Not Allowed to Navigate this Page',
+    "users" => "Users",
+    "user" => "User",
+    "admin" => "Admin",
+    "admins" => "Admins",
+    "super admin" => "Super Admin",
+    "super admins" => "Super Admins",
+    /* Todo : translate to arabic*/
+    "permissions" => "Permissions",
+    "permission" => "Permission",
 
 ];
